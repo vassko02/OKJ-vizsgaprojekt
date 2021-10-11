ad2a5af781903ebd480add60ee34ab26b028e1d8
ronczg <71878548+ronczg@users.noreply.github.com>
2021-10-11 09:33:35 +0200

    Add files via upload

--- vizsgaprojekt/script.js
+++ vizsgaprojekt/script.js
@@ -19,24 +19,66 @@ $(document).ready(function() {
   };
 
   function navigateUp() {
+    
     if (curPage === 1) return;
     curPage--;
     pagination();
   };
 
   function navigateDown() {
+    
     if (curPage === numOfPages) return;
     curPage++;
     pagination();
   };
 
   $(document).on("mousewheel DOMMouseScroll", function(e) {
+    console.log($(".skw-pages").position().top);
+    console.log($(window).scrollTop());
     if (scrolling) return;
-    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-      navigateUp();
-    } else { 
-      navigateDown();
+  
+      if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        if($(window).scrollTop() >= $(".skw-pages").position().top-1){
+        if(curPage != 1){
+          $('body').css({'overflow' : 'hidden'});
+          $(document).bind('scroll' , function(){
+          
+          });
+          
+        }
+        else{
+          $(document).unbind('scroll');
+          $('body').css({'overflow' : 'visible'});
+        }
+        navigateUp();
+      }
+      } else { 
+        if($(window).scrollTop() >= $(".skw-pages").position().top-10){
+          var a = document.createElement('a');
+          a.setAttribute('href', "#scrolling");
+          body.appendChild(a);
+          a.click();
+          body.removeChild(a);
+
+        
+        if(curPage != 5){
+          $('body').css({'overflow' : 'hidden'});
+          $(document).bind('scroll' , function(){
+           
+          });
+          
+        }
+        else{
+          $(document).unbind('scroll');
+          $('body').css({'overflow' : 'visible'});
+        }
+  
+        navigateDown();
+      
+      }
     }
+    
+   
   });
 
   $(document).on("keydown", function(e) {
