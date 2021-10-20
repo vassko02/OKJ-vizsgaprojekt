7782758eb97c1545af05784afb0d69530ba6248f
mauzileu <90752072+mauzileu@users.noreply.github.com>
2021-10-20 10:30:51 +0200

    Adatbázis v1.0

---
+++ SQL/peacefulparadise.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2021. Okt 20. 10:30
+-- Kiszolgáló verziója: 10.4.21-MariaDB
+-- PHP verzió: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `peacefulparadise`
+--
+CREATE DATABASE IF NOT EXISTS `peacefulparadise` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `peacefulparadise`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `consumption`
+--
+
+CREATE TABLE `consumption` (
+  `ConsumptionID` int(11) NOT NULL,
+  `Price` double NOT NULL,
+  `ItemName` varchar(50) NOT NULL,
+  `RoomID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `customer`
+--
+
+CREATE TABLE `customer` (
+  `CustomerID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `PhoneNumber` varchar(15) NOT NULL,
+  `IDNumber` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Customer data';
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `ReservationID` int(11) NOT NULL,
+  `GuestNumber` int(11) NOT NULL,
+  `Price` double NOT NULL,
+  `Children` int(11) NOT NULL,
+  `Adults` int(11) NOT NULL,
+  `ArrivalDate` date NOT NULL,
+  `LeavingDate` date NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `RoomID` int(11) NOT NULL,
+  `ServiceID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `room`
+--
+
+CREATE TABLE `room` (
+  `RoomID` int(11) NOT NULL,
+  `Capacity` int(11) NOT NULL,
+  `Description` varchar(300) NOT NULL,
+  `Price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `servicetype`
+--
+
+CREATE TABLE `servicetype` (
+  `ServiceID` int(11) NOT NULL,
+  `Price` double NOT NULL,
+  `ServiceType` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `consumption`
+--
+ALTER TABLE `consumption`
+  ADD PRIMARY KEY (`ConsumptionID`),
+  ADD KEY `RoomID` (`RoomID`);
+
+--
+-- A tábla indexei `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- A tábla indexei `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`ReservationID`),
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `RoomID` (`RoomID`),
+  ADD KEY `ServiceID` (`ServiceID`);
+
+--
+-- A tábla indexei `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`RoomID`);
+
+--
+-- A tábla indexei `servicetype`
+--
+ALTER TABLE `servicetype`
+  ADD PRIMARY KEY (`ServiceID`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `consumption`
+--
+ALTER TABLE `consumption`
+  MODIFY `ConsumptionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `ReservationID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `room`
+--
+ALTER TABLE `room`
+  MODIFY `RoomID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `servicetype`
+--
+ALTER TABLE `servicetype`
+  MODIFY `ServiceID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `consumption`
+--
+ALTER TABLE `consumption`
+  ADD CONSTRAINT `consumption_ibfk_1` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`);
+
+--
+-- Megkötések a táblához `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`),
+  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`ServiceID`) REFERENCES `servicetype` (`ServiceID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
