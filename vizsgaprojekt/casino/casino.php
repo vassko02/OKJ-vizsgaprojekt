fe888126926a678a1d09271f88c790efa92924ad
mauzileu <90752072+mauzileu@users.noreply.github.com>
2021-12-03 11:30:35 +0100

    Casino oldal továbbfejlesztése
    Header GIF és szöveg megvalósítása.

--- vizsgaprojekt/casino/casino.php
+++ vizsgaprojekt/casino/casino.php
@@ -1,5 +1,11 @@
-<div class="gif">
-    <h1>Casino</h1>
-    <h2>Welcome to our casino! The most luxurious thing you have ever seen!</h2>
+<div class="gifdiv">
+    <div class="cim">
+        <h1>PEACEFUL PARADISE</h1>
+        <h1>ROYALE CASINO</h1>
+    </div>
+    <h2>ELEVATE YOUR PLAY</h2>
 </div>
+
+
+
 
