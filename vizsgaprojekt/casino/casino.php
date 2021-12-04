acf006ea264ca9d567979add09a73ddfec77daca
mauzileu <90752072+mauzileu@users.noreply.github.com>
2021-12-04 13:27:14 +0100

    Casino oldal továbbfejlesztése
    A reszponzitás megvalósítása és a Game Cards hozzáadása.

--- vizsgaprojekt/casino/casino.php
+++ vizsgaprojekt/casino/casino.php
@@ -11,38 +11,65 @@
         <h4>Bet in our casino and change your life forever!</h4>
     </div>
     <div class="row basicinfo">
-        <div class="col-sm-6 col-md-6 col-lg-3 info">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 info">
             <div>
-                <p class="red"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi mb-2 bi-suit-club-fill" viewBox="0 0 16 16">
-  <path d="M11.5 12.5a3.493 3.493 0 0 1-2.684-1.254 19.92 19.92 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a19.919 19.919 0 0 0 1.582-2.907 3.5 3.5 0 1 1-2.538-5.743 3.5 3.5 0 1 1 6.708 0A3.5 3.5 0 1 1 11.5 12.5z"/>
-</svg><b> 125</b></p>
+                <p class="red"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi mb-2 bi-suit-club-fill" viewBox="0 0 16 16">
+                        <path d="M11.5 12.5a3.493 3.493 0 0 1-2.684-1.254 19.92 19.92 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a19.919 19.919 0 0 0 1.582-2.907 3.5 3.5 0 1 1-2.538-5.743 3.5 3.5 0 1 1 6.708 0A3.5 3.5 0 1 1 11.5 12.5z" />
+                    </svg><b> 125</b></p>
                 <p>Table games</p>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-3 info">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 info">
             <div>
-                <p class="green"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="mb-2 bi bi-coin" viewBox="0 0 16 16">
-  <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"/>
-  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
-</svg><b> 998</b></p>
+                <p class="green"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="mb-2 bi bi-coin" viewBox="0 0 16 16">
+                        <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z" />
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
+                    </svg><b> 998</b></p>
                 <p>Slot machines</p>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-3 info">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 info">
             <div>
-                <p class="red"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="mb-2 bi bi-tv-fill" viewBox="0 0 16 16">
-  <path d="M2.5 13.5A.5.5 0 0 1 3 13h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zM2 2h12s2 0 2 2v6s0 2-2 2H2s-2 0-2-2V4s0-2 2-2z"/>
-</svg><b> 12</b></p>
+                <p class="red"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="mb-2 bi bi-tv-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 13.5A.5.5 0 0 1 3 13h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zM2 2h12s2 0 2 2v6s0 2-2 2H2s-2 0-2-2V4s0-2 2-2z" />
+                    </svg><b> 12</b></p>
                 <p>Screens</p>
             </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-3 info">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-3 info">
             <div>
                 <p class="gold"><b>4550 m<sup>2</sup></b></p>
                 <p>Playground</p>
             </div>
         </div>
     </div>
-
+</div>
+<div class="games">
+    <div class="cards">
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
 </div>
