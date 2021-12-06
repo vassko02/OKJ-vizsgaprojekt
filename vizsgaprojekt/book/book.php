7a338261c60c4008a4f50d92dc9366fd010dff07
ronczg <71878548+ronczg@users.noreply.github.com>
2021-12-06 09:29:57 +0100

    book + restaurant

--- vizsgaprojekt/book/book.php
+++ vizsgaprojekt/book/book.php
@@ -1,177 +1,177 @@
-
-
-
-
-
-
-
-
-	<div class="book">
-		<div class="datas" id="box">
-			<form>
-				<div class="form-group div1 row">
-					<div class="first col">
-						<div class="second">
-							<div class="third">
-								<label class="col-form-label" for="adultsnumber">Adults number</label>
-							</div>
-							<div class="third">
-								<input class="form-control" type="number" id="adultsnumber" placeholder="Adults">
-							</div>
+<div class="book">
+	<div class="datas" id="box">
+		<form>
+			<div class="form-group div1 row">
+				<div class="first col">
+					<div class="second">
+						<div class="third">
+							<label class="col-form-label" for="adultsnumber">Adults number</label>
 						</div>
-					</div>
-					<div class="first col">
-						<div class="second">
-							<div class="third">
-								<label class="col-form-label" for="childrennumber">Children number</label>
-							</div>
-							<div class="third">
-								<input class="form-control" type="number" id="childrennumber" placeholder="Children">
-							</div>
-						</div>
-					</div>
-					<div class="first col">
-						<div class="second">
-							<div class="third">
-								<label class="col-form-label" for="roomnumber">Room number   </label>
-							</div>
-							<div class="third">
-								<input class="form-control" type="number" id="roomnumber" placeholder="Rooms">
-							</div>
+						<div class="third">
+							<input class="form-control" type="number" id="adultsnumber" placeholder="Adults">
 						</div>
 					</div>
 				</div>
-				<div class="form-group div2 row">
-					<div class="first col">
-						<div class="second">
-							<div class="third">
-								<label class="col-form-label" for="arrivaldate">Check-in</label>
-							</div>
-							<div class="third">
-								<input type="date" class="form-control " id="arrivaldate">
-							</div>
+				<div class="first col">
+					<div class="second">
+						<div class="third">
+							<label class="col-form-label" for="childrennumber">Children number</label>
 						</div>
-					</div>
-					<div class="first col">
-						<div class="second">
-							<div class="third">
-								<label class="col-form-label" for="leavingdate">Check-out</label>
-							</div>
-							<div class="third">
-								<input type="date" class="form-control " id="leavingdate">
-							</div>
+						<div class="third">
+							<input class="form-control" type="number" id="childrennumber" placeholder="Children">
 						</div>
 					</div>
 				</div>
-				<div class="form-group div3">
-					<!-- <button type="submit" class="btn btn-primary">Submit</button> -->
-					<button class="learn-more" id="button">
-						<span class="circle" aria-hidden="true">
-							<span class="icon arrow"></span>
-						</span>
-						<a class="button-text" href="">Search rooms</a>
-					</button>
+				<div class="first col">
+					<div class="second">
+						<div class="third">
+							<label class="col-form-label" for="roomnumber">Room number </label>
+						</div>
+						<div class="third">
+							<input class="form-control" type="number" id="roomnumber" placeholder="Rooms">
+						</div>
+					</div>
 				</div>
-			</form>
-		</div>
+			</div>
+			<div class="form-group div2 row">
+				<div class="first col">
+					<div class="second">
+						<div class="third">
+							<label class="col-form-label" for="arrivaldate">Check-in</label>
+						</div>
+						<div class="third">
+							<input type="date" class="form-control " id="arrivaldate">
+						</div>
+					</div>
+				</div>
+				<div class="first col">
+					<div class="second">
+						<div class="third">
+							<label class="col-form-label" for="leavingdate">Check-out</label>
+						</div>
+						<div class="third">
+							<input type="date" class="form-control " id="leavingdate">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="form-group div3">
+				<!-- <button type="submit" class="btn btn-primary">Submit</button> -->
+				<button class="learn-more" id="button">
+					<span class="circle" aria-hidden="true">
+						<span class="icon arrow"></span>
+					</span>
+					<a class="button-text" href="">Search rooms</a>
+				</button>
+			</div>
+		</form>
 	</div>
+</div>
 
-	<!-- 	 -->
-	<section class="dark">
-		<div class=" py-4">
-			<h1 class="h1 text-center" id="pageHeaderTitle">Rooms</h1>
-	
-			<article class="postcard dark blue">
-				<a class="postcard__img_link" href="#">
-					<img class="postcard__img" src="https://picsum.photos/1000/1000" alt="Image Title" />
-				</a>
-				<div class="postcard__text">
-					<h1 class="postcard__title blue"><a href="#">Podcast Title</a></h1>
-					<div class="postcard__subtitle small">
-						<time datetime="2020-05-25 12:00:00">
-							<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-						</time>
-					</div>
-					<div class="postcard__bar"></div>
-					<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-					<ul class="postcard__tagbox">
-						<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
-						<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
-						<li class="tag__item play blue">
-							<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
-						</li>
-					</ul>
-				</div>
-			</article>
-			<article class="postcard dark red">
-				<a class="postcard__img_link" href="#">
-					<img class="postcard__img" src="https://picsum.photos/501/500" alt="Image Title" />	
-				</a>
-				<div class="postcard__text">
-					<h1 class="postcard__title red"><a href="#">Podcast Title</a></h1>
-					<div class="postcard__subtitle small">
-						<time datetime="2020-05-25 12:00:00">
-							<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-						</time>
-					</div>
-					<div class="postcard__bar"></div>
-					<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-					<ul class="postcard__tagbox">
-						<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
-						<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
-						<li class="tag__item play red">
-							<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
-						</li>
-					</ul>
-				</div>
-			</article>
-			<article class="postcard dark green">
-				<a class="postcard__img_link" href="#">
-					<img class="postcard__img" src="https://picsum.photos/500/501" alt="Image Title" />
-				</a>
-				<div class="postcard__text">
-					<h1 class="postcard__title green"><a href="#">Podcast Title</a></h1>
-					<div class="postcard__subtitle small">
-						<time datetime="2020-05-25 12:00:00">
-							<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-						</time>
-					</div>
-					<div class="postcard__bar"></div>
-					<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-					<ul class="postcard__tagbox">
-						<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
-						<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
-						<li class="tag__item play green">
-							<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
-						</li>
-					</ul>
-				</div>
-			</article>
-			<article class="postcard dark yellow">
-				<a class="postcard__img_link" href="#">
-					<img class="postcard__img" src="https://picsum.photos/501/501" alt="Image Title" />
-				</a>
-				<div class="postcard__text">
-					<h1 class="postcard__title yellow"><a href="#">Podcast Title</a></h1>
-					<div class="postcard__subtitle small">
-						<time datetime="2020-05-25 12:00:00">
-							<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-						</time>
-					</div>
-					<div class="postcard__bar"></div>
-					<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-					<ul class="postcard__tagbox">
-						<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
-						<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
-						<li class="tag__item play yellow">
-							<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
-						</li>
-					</ul>
-				</div>
-			</article>
-		</div>
-	</section>
+<!-- 	 -->
+<section class="dark">
+	<div class=" py-4">
+		<h1 class="h1 text-center" id="pageHeaderTitle">Rooms</h1>
 
-
-
-	
+		<article class="postcard dark blue">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="./book/pictures/room1.jpeg" alt="Image Title" />
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title blue"><a href="#">Classic Room | 1 King | Non-Smoking</a></h1>
+				<div class="postcard__subtitle small">
+					<time datetime="2020-05-25 12:00:00">
+						<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+					</time>
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">One of our most popular rooms in the entire hotel, equipped with a 4K TV, a smart fridge, and a microwave. Outside on the balcony you will se on of the best sceneries that you have ever seen. The room has a classic look, with some modern touches to it. This is one of our non smoking rooms with unlimited Wifi access.</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Room</li>
+					<!-- <li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li> -->
+					<li class="tag__item play blue">
+						<a href="#"><i class="fas"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmarks-fill" viewBox="0 0 16 16">
+								<path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4z" />
+								<path d="M4.268 1A2 2 0 0 1 6 0h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L13 13.768V2a1 1 0 0 0-1-1H4.268z" />
+							</svg> Book now</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+		<article class="postcard dark red">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="./book/pictures/room3.jpg" alt="Image Title" />
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title red"><a href="#">Classic Room | 2 Kings | Non-Smoking</a></h1>
+				<div class="postcard__subtitle small">
+					<time datetime="2020-05-25 12:00:00">
+						<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+					</time>
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">One of our most popular rooms in the entire hotel, equipped with a 4K TV, a smart fridge, and a microwave. Outside on the balcony you will se on of the best sceneries that you have ever seen. The room has a classic look, with some modern touches to it. This is one of our non smoking rooms with unlimited Wifi access.</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Room</li>
+					<!-- <li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li> -->
+					<li class="tag__item play red">
+						<a href="#"><i class="fas"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmarks-fill" viewBox="0 0 16 16">
+								<path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4z" />
+								<path d="M4.268 1A2 2 0 0 1 6 0h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L13 13.768V2a1 1 0 0 0-1-1H4.268z" />
+							</svg> Book now</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+		<article class="postcard dark green">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="./book/pictures/room2.jpg" alt="Image Title" />
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title green"><a href="#">Exclusive Room | 1 King | Smoking</a></h1>
+				<div class="postcard__subtitle small">
+					<time datetime="2020-05-25 12:00:00">
+						<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+					</time>
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">One of our most exclusive rooms on the top floor of the hotel. This room is equipped with a lot of elite features, for example there is a jacuzzi installed in this room, in this room you are allowed to smoke, and you also have access to unlimited Wifi access. If you choose this room there will be waiting you with pre-chilled champagne in the fridge.</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Room</li>
+					<!-- <li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li> -->
+					<li class="tag__item play green">
+						<a href="#"><i class="fas"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmarks-fill" viewBox="0 0 16 16">
+								<path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4z" />
+								<path d="M4.268 1A2 2 0 0 1 6 0h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L13 13.768V2a1 1 0 0 0-1-1H4.268z" />
+							</svg> Book now</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+		<article class="postcard dark yellow">
+			<a class="postcard__img_link" href="#">
+				<img class="postcard__img" src="./book/pictures/room4.jpg" alt="Image Title" />
+			</a>
+			<div class="postcard__text">
+				<h1 class="postcard__title yellow"><a href="#">Premium Room | 1 King | Non-Smoking</a></h1>
+				<div class="postcard__subtitle small">
+					<time datetime="2020-05-25 12:00:00">
+						<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
+					</time>
+				</div>
+				<div class="postcard__bar"></div>
+				<div class="postcard__preview-txt">A premium room for premium guests. The room is equipped with a movie theather that you can watch enything on. The design of this room is made by one of our finest designers, to make sure everyone feels comfortable staying here. When sitting on the balcony cou can see the best view you have ever seen in your entire life.</div>
+				<ul class="postcard__tagbox">
+					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Room</li>
+					<!-- <li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li> -->
+					<li class="tag__item play yellow">
+						<a href="#"><i class="fas"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmarks-fill" viewBox="0 0 16 16">
+								<path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4z" />
+								<path d="M4.268 1A2 2 0 0 1 6 0h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L13 13.768V2a1 1 0 0 0-1-1H4.268z" />
+							</svg> Book now</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+	</div>
+</section>
