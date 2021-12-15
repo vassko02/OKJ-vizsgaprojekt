47ba0b3e1a1ebfeec05431587221d829c800e0ce
mauzileu <90752072+mauzileu@users.noreply.github.com>
2021-12-15 17:57:22 +0100

    Casino bugfixes

--- vizsgaprojekt/casino/casino.php
+++ vizsgaprojekt/casino/casino.php
@@ -49,21 +49,21 @@
         <div class="games col-12 col-sm-12 col-md-12 col-lg-12">
             <div class="row gamesitems">
                 <div class="customcard1 col-12 col-sm-6 col-md-6 col-lg-4">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="pictures\casino\blackjack1.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h1 class="card-title">BLACKJACK</h1>
                         <p class="card-text">Of all the casino table games, there is none more classic than Blackjack, also known as 21. It uses decks of 52 cards. Hit, stand, split and double - get closer than the dealer to 21 without going over and win big.</p>
                     </div>
                 </div>
                 <div class="customcard2 col-12 col-sm-6 col-md-6 col-lg-4">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="pictures/casino/roulette1.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h1 class="card-title">ROULETTE</h1>
                         <p class="card-text">The purest game of chance, Roulette gives players a chance to test their luck by picking numbers on a wheel, choosing from 1 to 36, with a 0 and 00. Roulette offers many ways to win - betting individual numbers, intersections of up to five numbers at once, odd or even, red or black.</p>
                     </div>
                 </div>
                 <div class="customcard3 col-12 col-sm-6 col-md-6 col-lg-4">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="pictures/casino/craps1.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h1 class="card-title">CRAPS</h1>
                         <p class="card-text">When the dice roll, something special happens. The whole table can win together, making Craps a game with a social component like no other. With numerous betting options and specialized lingo, after a few minutes even first-timers get into the spirit.</p>
