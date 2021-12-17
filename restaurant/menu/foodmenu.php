61f62654a61d69bec6d333b064c05a5fe573453a
ronczg <71878548+ronczg@users.noreply.github.com>
2021-12-17 13:16:11 +0100

    contacc

---
+++ restaurant/menu/foodmenu.php
@@ -0,0 +1,220 @@
+<div class="bodyy">
+    <div>
+        <div class="eloetelcim">
+            <h1>Appetizers</h1>
+        </div>
+        <div class="container-fluid">
+            <div class="cardss row mx-auto">
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body mx-auto">
+                        <img src="restaurant/menu/pictures/cranberry.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Cranberry Brie Bites</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/garlicbread.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Cheesy Garlic Pull-Apart Bread</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/potato.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Sweet Potato Bites</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/AntipastoBites.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Antipasto Bites</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/JalapeñoCheeseCrisps.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Jalapeño Cheese Crisps</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/ShrimpCocktail.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Shrimp Cocktail</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div>
+        <div class="levescim">
+            <h1>Soups</h1>
+        </div>
+        <div class="container-fluid">
+        <div class="cardss row mx-auto">
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/ButternutSquashSoupWithBacon.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Butternut Squash Soup With Bacon</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/CreamofAsparagusSoup.jpeg" class="card-img-top" alt="...">
+                        <p class="card-text">Cream of Asparagus Soup</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/FrenchOnionPotatoSoup.jpeg" class="card-img-top" alt="...">
+                        <p class="card-text">French Onion Potato Soup</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/HomemadeChickenNoodleSoup.jpeg" class="card-img-top" alt="...">
+                        <p class="card-text">Homemade Chicken Noodle Soup</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/CreamyChickenandWildRiceSoup.jpeg" class="card-img-top" alt="...">
+                        <p class="card-text">Creamy Chicken and Wild Rice Soup</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/BroccoliCheeseSoup.jpeg" class="card-img-top" alt="...">
+                        <p class="card-text">Broccoli Cheese Soup</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="foetelcim">
+            <h1>Main Courses</h1>
+        </div>
+        <div class="container-fluid">
+        <div class="cardss row mx-auto">
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/cedarplank.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Cedar-Plank Salmon</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/braisedchicken.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Braised Chicken Legs With Grapes and Fennel</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/searedscallops.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Seared Scallops With Brown Butter and Lemon Pan Sauce</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/pizza.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Grilled Pizza</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/burgir.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Double-Stack Mushroom and Chicken Cheeseburgers</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/gnocchi.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Quick Potato Gnocchi</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/lasagna.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Broken Lasagna With Parmesan and All the Peas</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/bbqsh.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Habanero BBQ Shrimp</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/pork.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Grilled Pork Spareribs With Soda Bottle Barbecue Sauce</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/caesarsalad.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Caesar Salad Roast Chicken</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/cocnutshrimp.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Grilled Coconut Shrimp With Shishito Peppers</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/gadogado.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Gado Gado</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div>
+        <div class="dessertcim">
+            <h1>Desserts</h1>
+        </div>
+  
+        <div class="cardss row mx-auto">
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/1.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Ginger Layer Cake with Wine Poached Pears and Cream Cheese Frosting </p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/2.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Matchamisu - Matcha (Green Tea) Tiramisu </p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/3.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Chocolate Covered Strawberry Mini Cheesecakes</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/4.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Blueberry Lavender Mini Cheesecakes</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/5.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Wine Soaked Chocolate Covered Cherries</p>
+                    </div>
+                </div>
+                <div class="card col-lg-2 col-md-4 col-sm-12">
+                    <div class="card-body">
+                        <img src="restaurant/menu/pictures/6.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Lemon Blueberry Trifle</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
---
+++ restaurant/restaurant.php
@@ -0,0 +1,53 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+
+<body>
+  <div class="hatter">
+
+    <div class="title">
+
+      <h1>Welcome to our restaurant!</h1>
+    </div>
+
+
+  </div>
+  <div class="favoritefood">
+  <p>Take a look at our best seller meals</p>
+  </div>  
+  <div class="row">
+    <div class="card col-lg-2 col-md-4 col-sm-12">
+    <div class="card-body">
+      <img src="restaurant/menu/pictures/garlicbread.jpg" class="card-img-top" alt="...">
+      <p class="card-text">Cheesy Garlic Pull-Apart Bread</p>
+    </div>
+    </div>
+    <div class="card col-lg-2 col-md-4 col-sm-12">
+    <div class="card-body">
+                        <img src="restaurant/menu/pictures/CreamyChickenandWildRiceSoup.jpeg" class="card-img-top" alt="...">
+                        <p class="card-text">Creamy Chicken and Wild Rice Soup</p>
+                    </div>
+    </div>
+    <div class="card col-lg-2 col-md-4 col-sm-12">
+    <div class="card-body">
+                        <img src="restaurant/menu/pictures/searedscallops.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Seared Scallops With Brown Butter and Lemon Pan Sauce</p>
+                    </div>
+    </div>
+    <div class="card col-lg-2 col-md-4 col-sm-12">
+      <div class="card-body">
+                        <img src="restaurant/menu/pictures/2.jpg" class="card-img-top" alt="...">
+                        <p class="card-text">Matchamisu - Matcha (Green Tea) Tiramisu </p>
+                    </div>
+    </div>
+    <div class="card col-lg-2 col-md-4 col-sm-12">
+      <div class="card-body">
+        <img class="card-img-top" src="./restaurant/foodpics/menuitem5.jpg" alt="Card image cap">
+        <p class="card-text"> Juicy Lucy Burger</p>
+      </div>
+    </div>
+  </div>
+  <div class="menu">
+    <a class=" btn w-100" href="index.php?m=foodmenu">Menu</a>
+  </div>
+</body>
