4a445e25042af5538f39a883f131b4b9273a99dc
ronczg <71878548+ronczg@users.noreply.github.com>
2021-12-20 08:06:58 +0100

    contacc

--- vizsgaprojekt/contact/contact.php
+++ vizsgaprojekt/contact/contact.php
@@ -1,3 +1,71 @@
-
-<h1>Contact us!</h1>
-
+<div class="contact">
+    <div class="urlap mx-auto">
+        <h1 class="text-white">
+            Contact us!
+        </h1>
+        <form class="row g-3 needs-validation text-white" novalidate>
+  <div class="col-md-4 text-white" >
+    <label for="validationCustom01" class="form-label">First name</label>
+    <input type="text" class="form-control" id="validationCustom01" value="" required>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  </div>
+  <div class="col-md-4">
+    <label for="validationCustom02" class="form-label">Last name</label>
+    <input type="text" class="form-control" id="validationCustom02" value="" required>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  </div>
+  <div class="col-md-4">
+    <label for="validationCustomemail" class="form-label">Email</label>
+    <div class="input-group has-validation">
+      <input type="email" class="form-control" id="validationCustomemail" aria-describedby="inputGroupPrepend" required>
+      <div class="invalid-feedback">
+        Please provide a valid email.
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <label for="validationCustom03" class="form-label">City</label>
+    <input type="text" class="form-control" id="validationCustom03" required>
+    <div class="invalid-feedback">
+      Please provide a valid city.
+    </div>
+  </div>
+  <div class="col-md-3">
+    <label for="validationCustom04" class="form-label">State</label>
+    <select class="form-select" id="validationCustom04" required>
+      <option selected disabled value="">Choose...</option>
+      <option>...</option>
+    </select>
+    <div class="invalid-feedback">
+      Please select a valid state.
+    </div>
+  </div>
+  <div class="col-md-3">
+    <label for="validationCustom05" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="validationCustom05" required>
+    <div class="invalid-feedback">
+      Please provide a valid zip.
+    </div>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
+        Agree to terms and conditions
+      </label>
+      <div class="invalid-feedback">
+        You must agree before submitting.
+      </div>
+    </div>
+  </div>
+  <div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+  </div>
+</form>
+    </div>
+</div>
+<script src="./contact/contact.js"></script>
