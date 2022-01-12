d1b4933489c915674734becb192648c6a04aef98
mauzileu <kaldavai26@gmail.com>
2022-01-12 13:55:39 +0100

    Navbar dropdown & 404 page bugfix

--- vizsgaprojekt/navbar/navbar.php
+++ vizsgaprojekt/navbar/navbar.php
@@ -8,11 +8,11 @@
         <div class="line3"></div>
     </div>
     <ul class="navlinks">
-        <li><a href="index.php?m=home">Home</a></li>
-        <li><a href="index.php?m=casino">Casino</a></li>
-        <li><a href="index.php?m=book">Book</a></li>
-        <li><a href="index.php?m=restaurant">Restaurant</a></li>
-        <li><a href="index.php?m=contactus">Contact Us</a></li>
+        <li><a href="index.php?m=home" class="navlink">Home</a></li>
+        <li><a href="index.php?m=casino" class="navlink">Casino</a></li>
+        <li><a href="index.php?m=book" class="navlink">Book</a></li>
+        <li><a href="index.php?m=restaurant" class="navlink">Restaurant</a></li>
+        <li><a href="index.php?m=contactus" class="navlink">Contact Us</a></li>
         <li class="lenyilo">
             <div class="dropdown">
                 <button class="btn btn-dark dropdown-toggle lenyilogomb" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
@@ -21,7 +21,7 @@
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                     <li><a class="dropdown-item" href="index.php?m=regisztracio"><i class="fas fa-user-plus"></i> Sign up</a></li>
                     <li><a class="dropdown-item" href="index.php?m=signin"><i class="fas fa-sign-in-alt"></i> Sign in</a></li>
-                    <li><a class="dropdown-item" href="index.php?m=logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                    <li><a class="dropdown-item" href="index.php?m=home"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
                 </ul>
             </div>
         </li>
