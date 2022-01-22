f994d5c394f28f38039f108e6657ed5b9b1c7168
mauzileu <kaldavai26@gmail.com>
2022-01-22 20:23:37 +0100

    Restaurant bugfix

--- vizsgaprojekt/index.php
+++ vizsgaprojekt/index.php
@@ -149,6 +149,9 @@ if (isset($_POST['btn_send2'])) {
     if ($request === $baseUrl . '/dining/menu') {
         echo '
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+                <link rel="preconnect" href="https://fonts.googleapis.com">
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
                 <link rel="stylesheet" href="../restaurant/menu/foodmenu.css">
                 ';
     }
@@ -302,13 +305,13 @@ if (isset($_POST['btn_send2'])) {
 
 
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-    <script src="<?php if ($mennyiper === 5) {
-                        echo '.';
-                    } else if ($mennyiper === 6) {
-                        echo '../.';
-                    }
-                    if ($mennyiper === 7) {
-                        echo '../../.';
+    <script src="<?php if ($mennyiper === ($baseMennyiper+2)) {
+                                        echo '.';
+                                    } else if ($mennyiper === ($baseMennyiper+3)) {
+                                        echo '../.';
+                                    }
+                                    else if ($mennyiper === ($baseMennyiper+4)) {
+                                        echo '../../.';
                     } ?>./navbar/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
