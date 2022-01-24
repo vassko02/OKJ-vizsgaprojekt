90fc398c3d5ee4c636c09ce6585b24447d35d626
mauzileu <kaldavai26@gmail.com>
2022-01-24 13:39:07 +0100

    Email config

--- vizsgaprojekt/models/email/mail.php
+++ vizsgaprojekt/models/email/mail.php
@@ -10,10 +10,20 @@ class mail extends Dbconnect
         // Message
         $message = '<html>
         <head>
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro&display=swap" rel="stylesheet">
           <title>Reservation confirment</title>
         </head>
         <body>
-          <img style="width: 100%;" src="http://diak.jedlik.eu/~PeacefulParadise/pictures/korulvagott_logo.png" alt="">
+          <div style="background-color: black; max-width: 600px; margin-left: auto; margin-right: auto; padding-bottom: 30px;">
+            <img style="width: 100%;" src="http://diak.jedlik.eu/~PeacefulParadise/pictures/korulvagott_logo.png" alt="">
+            <div style="text-align: center; width: 100%; color: #C5B358; background-color: black;">
+              <h1 style="font-family: "Be Vietnam Pro", sans-serif; font-weight: normal;">Thanks you for your Reservation!</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultrices dolor non neque imperdiet, sed commodo ligula pretium. Sed quis ipsum vel orci hendrerit convallis. Sed cursus turpis non urna tristique, ut cursus elit tincidunt. Sed accumsan diam eu dui ornare, tincidunt accumsan metus finibus. Vestibulum sodales elementum lacinia. In ultrices congue velit, id sollicitudin nunc bibendum at. Nunc non tortor purus. Quisque sit amet consequat nibh, ac porttitor arcu. Duis blandit sem magna. Vestibulum tempus ex ac scelerisque efficitur. Ut molestie turpis id ante finibus, ut hendrerit est mattis. In rhoncus mauris arcu, id lacinia elit ornare quis. Pellentesque consectetur mi ac tellus accumsan, sed rhoncus urna cursus. Nullam porttitor mauris dolor, vel venenatis neque rhoncus ac. Donec imperdiet augue ligula, ut gravida nunc mattis a.</p>
+            </div>
+          </div>
+          
         </body>
         </html>';
 
