e5fdef41b7d892e4b5c37efa3abf51bc473baca1
mauzileu <kaldavai26@gmail.com>
2022-01-26 13:14:58 +0100

    Email verification design&bugfix + navbar bugfix

--- vizsgaprojekt/aktivalas/aktivalas.php
+++ vizsgaprojekt/aktivalas/aktivalas.php
@@ -5,17 +5,23 @@
 
             $user = $GuestObj->find_unverified_user($_GET['activation_code'], $_GET['email']);
             if ($user === null) { //rossz az email cim vagy a kod
-                echo '<h2>Invalid link!</h2>
+                echo '
+                    <i id="error" class="far fa-times-circle"></i>
+                    <h1>Invalid link!</h1>
                     <p>The account activation has been failed or it has been already activated</p>
                     <a href="'.$baseUrl.'/">Back to the main page</a>';
             } else {
                 if (md5($_GET['activation_code']) === $user['activation_code']) { //egyezik a kod es van ilyen user -> nincs hiba
                     $GuestObj->activate_user($user['CustomerID']);
-                    echo '<h2>Successful activation!</h2>
+                    echo '
+                    <i id="success" class="far fa-check-circle"></i>
+                    <h1>Successful activation!</h1>
                     <p>Now you can sign in and get full access on our website</p>
                     <a href="'.$baseUrl.'/signin">Sign in now</a>';
                 } else { //rossz az email cim vagy a kod
-                    echo '<h2>Invalid link!</h2>
+                    echo '
+                    <i id="error" class="far fa-times-circle"></i>
+                    <h1>Invalid link!</h1>
                     <p>The account activation has been failed or it has been already activated</p>
                     <a href="'.$baseUrl.'/">Back to the main page</a>';
                 }
@@ -26,7 +32,9 @@
             //echo '<br>';
         } 
         else { //az a link
-            echo '<h2>Invalid link!</h2>
+            echo '
+            <i id="error" class="far fa-times-circle"></i>
+            <h1>Invalid link!</h1>
             <p>The account activation has been failed or it has been already activated</p>
             <a href="'.$baseUrl.'/">Back to the main page</a>';
         }
