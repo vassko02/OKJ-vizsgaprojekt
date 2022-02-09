c0d9d38c6f715c1cddcc18c436350b5cbe1c9a52
mauzileu <kaldavai26@gmail.com>
2022-02-09 13:50:35 +0100

    Navbar message fül + reservations aloldal bugfix

--- vizsgaprojekt/login/guestreservations.php
+++ vizsgaprojekt/login/guestreservations.php
@@ -1,30 +1,30 @@
 <?php
-  defined('VEDETT') or die(' ísx');
+defined('VEDETT') or die(' ísx');
 ?>
 <div class="row">
-   <?php
-    if (isset($_POST['deletee'])) {
-      $_SESSION['reservationid'] = $_POST['reservationid']; 
-      $ReservationObj->deleteconsumption($_SESSION['reservationid']);
-      $ReservationObj->deletereservation($_SESSION['reservationid']);
-    }
-    $empty = false;
-    $reservations[] = $ReservationObj->selectallreservation();
-    $allrooms = $RoomObj->selectallrooms();
-    $services = $ServiceObj->getallservice();
-    $counter = 0;
-    $counter2 = 0;
-    $user =  $GuestObj->getuserbyid($_SESSION['username']);
-    $today = date("Y-m-d");
-    
-    foreach ($reservations as $onereservation) {
+  <?php
+  if (isset($_POST['deletee'])) {
+    $_SESSION['reservationid'] = $_POST['reservationid'];
+    $ReservationObj->deleteconsumption($_SESSION['reservationid']);
+    $ReservationObj->deletereservation($_SESSION['reservationid']);
+  }
+  $empty = false;
+  $reservations[] = $ReservationObj->selectallreservation();
+  $allrooms = $RoomObj->selectallrooms();
+  $services = $ServiceObj->getallservice();
+  $counter = 0;
+  $counter2 = 0;
+  $user =  $GuestObj->getuserbyid($_SESSION['username']);
+  $today = date("Y-m-d");
+
+  foreach ($reservations as $onereservation) {
     foreach ($onereservation as $oneonereservation) {
-    if ($oneonereservation['CustomerID'] == $user['CustomerID']) {
-     $roomname =  $RoomObj->selectoneroomname($oneonereservation['RoomID']);
-     $servicee = $ServiceObj->getservicebyid($oneonereservation['ServiceID']);
-                $room[] = $RoomObj->selectoneroom2($oneonereservation['RoomID']);
-            echo '
-       <div  class="container" id="container">
+      if ($oneonereservation['CustomerID'] == $user['CustomerID']) {
+        $roomname =  $RoomObj->selectoneroomname($oneonereservation['RoomID']);
+        $servicee = $ServiceObj->getservicebyid($oneonereservation['ServiceID']);
+        $room[] = $RoomObj->selectoneroom2($oneonereservation['RoomID']);
+        echo '
+       <div class="container" id="container">
        <form id="reservation" class="neon1" action="" method="POST">
          <h3>' . $oneonereservation['Name'] . '</h3>
          <h4 class="resemail">' . $oneonereservation['Email'] . '</h4>
@@ -35,7 +35,7 @@
          <input type="hidden" name="price" value="' . $oneonereservation['Price'] . '"/>
          <fieldset>
          <h4 for="">Room type</h4>
-         <p class="form-control">'.$roomname['RoomName'].'</p>
+         <p class="form-control">' . $roomname['RoomName'] . '</p>
     
     
     
@@ -43,18 +43,18 @@
          </fieldset>
          <fieldset>
          <h4 for="">Service type</h4>
-         <p class="form-control">'.$servicee[0]['ServiceType'].'</p>
+         <p class="form-control">' . $servicee[0]['ServiceType'] . '</p>
          </fieldset>
          <fieldset>
           <h4 for="">Check in - Check out</h4>
-          <p class="form-control" tabindex="2">'. $oneonereservation['ArrivalDate'].'</p>
-          <p class="form-control" tabindex="3">'. $oneonereservation['LeavingDate'] .'</p>
+          <p class="form-control" tabindex="2">' . $oneonereservation['ArrivalDate'] . '</p>
+          <p class="form-control" tabindex="3">' . $oneonereservation['LeavingDate'] . '</p>
          </fieldset>
          <fieldset>
           <h4 for="">Adults</h4>
-          <p class="form-control" tabindex="3">'. $oneonereservation['Adults'] .'</p>
+          <p class="form-control" tabindex="3">' . $oneonereservation['Adults'] . '</p>
           <h4 for="">Children</h4>
-          <p class="form-control" tabindex="3">'. $oneonereservation['Children'] .'</p>
+          <p class="form-control" tabindex="3">' . $oneonereservation['Children'] . '</p>
          </fieldset>
          <fieldset>
           <h4 for="">Message</h4>
@@ -62,51 +62,47 @@
          </fieldset>
          <fieldset>
          ';
-         if ($today < $oneonereservation['ArrivalDate']  ) {
-             echo '<a href="'.$baseUrl.'/delete" class="btn btn-danger w-100 delet"   data-bs-toggle="modal" data-bs-target="#deleteconfirm">Delete</a>';
-           
-         }
-         echo'
+        if ($today < $oneonereservation['ArrivalDate']) {
+          echo '<a href="' . $baseUrl . '/delete" class="btn btn-danger w-100 delet"   data-bs-toggle="modal" data-bs-target="#deleteconfirm">Delete</a>';
+        }
+        echo '
          </fieldset>
          <div class="modal fade text-dark"  id="deleteconfirm" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-dark" id="staticBackdropLabel" >Please confirm</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            Are you sure you want to delete?
-          </div>
-          <div class="modal-footer">
-            <button  class="btn btn-secondary w-25 no" data-bs-dismiss="modal">No</button>
-            <button name="deletee" type="submit" class="btn btn-primary w-25 deletee" id="btnYes">Yes</button>
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title text-dark" id="staticBackdropLabel" >Please confirm</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              Are you sure you want to delete?
+            </div>
+            <div class="modal-footer">
+              <button  class="btn btn-secondary w-25 no" data-bs-dismiss="modal">No</button>
+              <button name="deletee" type="submit" class="btn btn-primary w-25 deletee" id="btnYes">Yes</button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-       </form>
-    
-    
-     </div>
+        </div>
+          </form>
+        
+        
+        </div>
     
        ';
-            }
-        else{
-            $empty = true;
-        }
-      
-        $counter += 1;
+       $empty = true;
+      } 
+
+      $counter += 1;
     }
-    }
-    if ($empty == true) {
-      echo '
+  }
+  if ($empty == false) {
+    echo '
       <div class="donthaveres">
   <h1 id="nagytitle">You do not have any reservation at the moment!</h1>
-  <h4><a href="'.$baseUrl.'/booking">Make a reservation...</a></h4>
+  <h4><a href="' . $baseUrl . '/booking">Make a reservation...</a></h4>
   </div>
   ';
-  
   }
-?>
- </div>
+  ?>
+</div>
--- vizsgaprojekt/navbar/navbar.php
+++ vizsgaprojekt/navbar/navbar.php
@@ -12,52 +12,64 @@
         <li><a href="<?php echo $baseUrl ?>/gambling" class="navlink">Casino</a></li>
         <li><a href="<?php echo $baseUrl ?>/booking" class="navlink">Book</a></li>
         <li><a href="<?php echo $baseUrl ?>/dining" class="navlink">Restaurant</a></li>
-        <li><a href="<?php echo $baseUrl ?>/contactus" class="navlink">Contact Us</a></li>
-        <li><a href="<?php echo $baseUrl ?>/rating" class="navlink">Rate us</a></li>
-        <li class="lenyilo">
-            <div class="dropdown">
-                <button class="dropdown-toggle lenyilogomb" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fas fa-user-circle"></i>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                    <li><a class="dropdown-item leftborder" href="' . $baseUrl . '/signin"><i class="fas fa-user-plus"></i>Contact</a></li>
-                </ul>
+        <li class="d-flex lenyilo">
+            <div class="lenyilo">
+                <div class="dropdown">
+                    <button class="dropdown-toggle lenyilogomb" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-comment-dots"></i>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                        <li><a class="dropdown-item leftborder" href="<?php echo $baseUrl; ?>/contactus"><i class="fas fa-envelope"></i> Contact Us</a></li>
+                        <li><a class="dropdown-item leftborder" href="<?php echo $baseUrl; ?>/rating"><i class="fas fa-star-half-alt"></i> Rate Us</a></li>
+                    </ul>
+                </div>
             </div>
-        </li>
-        <li class="lenyilo">
-            <div class="dropdown">
-                <button class="dropdown-toggle lenyilogomb" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="fas fa-user-circle"></i>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <?php if (isset($_SESSION['username'])) {
-                    } else {
-                        echo '<li><a class="dropdown-item leftborder" href="' . $baseUrl . '/signin"><i class="fas fa-user-plus"></i> Sign in</a></li>';
-                    }
-                    ?>
-                    <?php
-                    if (isset($_SESSION['username']) && $_SESSION['isadmin'] != '1') {
-                        echo '
+            <div class="lenyilo">
+                <div class="dropdown">
+                    <button class="dropdown-toggle lenyilogomb" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fas fa-user-circle"></i>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <?php if (isset($_SESSION['username'])) {
+                        } else {
+                            echo '<li><a class="dropdown-item leftborder" href="' . $baseUrl . '/signin"><i class="fas fa-user-plus"></i> Sign in</a></li>';
+                        }
+                        ?>
+                        <?php
+                        if (isset($_SESSION['username']) && $_SESSION['isadmin'] != '1') {
+                            echo '
                             <li class="leftborder"><a class="dropdown-item" href="' . $baseUrl . '/account"><i class="fas fa-user-cog"></i> My account</a></li>
                             <li class="leftborder"><a class="dropdown-item" href="' . $baseUrl . '/guestreservations"><i class="fas fa-concierge-bell"></i> My reservations</a></li>
                             <div class="dropdown-divider"></div>
                             <li class="dropdown-item" id="username">Logged in as: <span class="gold">' . $_SESSION['username'] . '</span><li>
                             <li class="leftborder"><a class="dropdown-item" href="' . $baseUrl . '/logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
                             ';
-                    }
-                    if (isset($_SESSION['username']) && $_SESSION['isadmin'] == '1') {
-                        echo '
+                        }
+                        if (isset($_SESSION['username']) && $_SESSION['isadmin'] == '1') {
+                            echo '
                             <li class="leftborder"><a class="dropdown-item" href="' . $baseUrl . '/admin"><i class="fas fa-tasks"></i> Manage reservations</a></li>
-                            <li class="leftborder"><a class="dropdown-item" href="' . $baseUrl . '/userseditadmin"><i class="fas fa-tasks"></i> Manage users</a></li>
+                            <li class="leftborder"><a class="dropdown-item" href="' . $baseUrl . '/userseditadmin"><i class="fas fa-users-cog"></i> Manage users</a></li>
                             <div class="dropdown-divider"></div>
                             <li class="dropdown-item" id="username">Logged in as: <span class="gold">' . $_SESSION['username'] . '</span><li>
                             <li class="leftborder"><a class="dropdown-item" href="' . $baseUrl . '/logout"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
                             ';
-                    }
-                    ?>
-                </ul>
+                        }
+                        ?>
+                    </ul>
+                </div>
             </div>
         </li>
+        <div class="eltuno">
+        <?php 
+                if (isset($_SESSION['isadmin']) && $_SESSION['isadmin'] == '1') {
+                    
+                }
+                else {
+                    echo '<li><a class="navlink" href="<?php echo $baseUrl; ?>/contactus"><i class="fas fa-envelope"></i> Contact Us</a></li>
+                    <li><a class="navlink" href="<?php echo $baseUrl; ?>/rating"><i class="fas fa-star-half-alt"></i> Rate Us</a></li>';
+                }
+            ?>
+        </div>
         <div class="eltuno">
             <?php if (isset($_SESSION['username'])) {
             } else {
@@ -67,13 +79,13 @@
             <?php if (isset($_SESSION['username'])) {
                 if (isset($_SESSION['username']) && $_SESSION['isadmin'] == '1') {
                     echo '
-                    <li><a class="navlink" href="' . $baseUrl . '/admin"><i class="fas fa-sign-out-alt"></i> Manage reservations</a></li>
-                    <li><a class="navlink" href="' . $baseUrl . '/userseditadmin"><i class="fas fa-sign-out-alt"></i>  Manage users</a></li>';
+                    <li><a class="navlink" href="' . $baseUrl . '/admin"><i class="fas fa-tasks"></i> Manage reservations</a></li>
+                    <li><a class="navlink" href="' . $baseUrl . '/userseditadmin"><i class="fas fa-users-cog"></i>  Manage users</a></li>';
                 }
                 if (isset($_SESSION['username']) && $_SESSION['isadmin'] != '1') {
 
                     echo '
-                    <li><a class="navlink" href="' . $baseUrl . '/account"><i class="fas fa-user-cog"></i>  My account</a></li>
+                    <li><a class="navlink" href="' . $baseUrl . '/account"><i class="fas fa-users-cog"></i>  My account</a></li>
                     <li><a class="navlink" href="' . $baseUrl . '/guestreservations"><i class="fas fa-concierge-bell"></i> My reservations</a></li>';
                 }
                 echo '
